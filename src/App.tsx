7b6c710c215e5cabe6bac7b9dc4f65a42f0bb059
tsubasa <tsubasa.y1231@gmail.com>
2021-09-21 21:35:17 +0900

    stop call api

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,43 @@
 import React, { useState, useEffect } from "react"
-import { UserList } from "./components/UserList"
-import { UserForm } from "./components/UserForm"
+// import { UserList } from "./components/UserList"
+// import { UserForm } from "./components/UserForm"
 
-import { getUsers } from "./lib/api/users"
-import { User} from "./interfaces"
+// import { getUsers } from "./lib/api/users"
+// import { User} from "./interfaces"
 
 const App: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([])
+  // const [users, setUsers] = useState<User[]>([])
 
-  const handleGetUsers = async () => {
-    try {
-      const res = await getUsers()
-      console.log(res)
+  // const handleGetUsers = async () => {
+  //   try {
+  //     const res = await getUsers()
+  //     console.log(res)
 
-      if (res?.status === 200) {
-        setUsers(res.data.users)
-      } else {
-        console.log(res.data.message)
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  //     if (res?.status === 200) {
+  //       setUsers(res.data.users)
+  //     } else {
+  //       console.log(res.data.message)
+  //     }
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
-  useEffect(() => {
-    handleGetUsers()
-  }, [])
+  // useEffect(() => {
+  //   handleGetUsers()
+  // }, [])
 
+  // return (
+  //     <>
+  //       <h1>User App</h1>
+  //       {/* <UserForm users={users} setUsers={setUsers} />
+  //       <UserList users={users} setUsers={setUsers} /> */}
+  //     </>
+  // )
   return (
-      <>
-        <h1>User App</h1>
-        <UserForm users={users} setUsers={setUsers} />
-        <UserList users={users} setUsers={setUsers} />
-      </>
+    <>
+      <h1>User App</h1>
+    </>
   )
 }
 
